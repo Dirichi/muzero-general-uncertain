bf9c8ae4922ddb7661e1a6bd5f87121c8b14c244
Dirichi <din206@nyu.edu>
2022-08-29 19:58:55 +0100

    Show consistency loss

--- muzero.py
+++ muzero.py
@@ -322,6 +322,7 @@ class MuZero:
                 writer.add_scalar("3.Loss/Value_loss", info["value_loss"], counter)
                 writer.add_scalar("3.Loss/Reward_loss", info["reward_loss"], counter)
                 writer.add_scalar("3.Loss/Policy_loss", info["policy_loss"], counter)
+                writer.add_scalar("3.Loss/Consistency_Loss", info["consistency_loss"], counter)
                 writer.add_scalar("4.Uncertainty/Average_uncertainty", info["uncertainty"], counter)
                 print(
                     f'Last test reward: {info["total_reward"]:.2f}. Training step: {info["training_step"]}/{self.config.training_steps}. Played games: {info["num_played_games"]}. Loss: {info["total_loss"]:.2f}',
