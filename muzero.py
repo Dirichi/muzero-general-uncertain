b164147e4ecd768931b88212d13242f31fd4198f
Dirichi <din206@nyu.edu>
2022-08-29 21:21:41 +0100

    Fix typo

--- muzero.py
+++ muzero.py
@@ -327,7 +327,7 @@ class MuZero:
                 writer.add_scalar("3.Loss/Reward_loss", info["reward_loss"], counter)
                 writer.add_scalar("3.Loss/Policy_loss", info["policy_loss"], counter)
                 writer.add_scalar("3.Loss/Consistency_Loss", info["consistency_loss"], counter)
-                writer.add_scalar("3.Loss/Diversity_Loss", info["diversity_Loss"], counter)
+                writer.add_scalar("3.Loss/Diversity_Loss", info["diversity_loss"], counter)
                 writer.add_scalar("4.Uncertainty/Average_uncertainty", info["uncertainty"], counter)
                 print(
                     f'Last test reward: {info["total_reward"]:.2f}. Training step: {info["training_step"]}/{self.config.training_steps}. Played games: {info["num_played_games"]}. Loss: {info["total_loss"]:.2f}',
