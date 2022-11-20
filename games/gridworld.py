168099a485c6ea36fbfe8c37b049ccd18d719fd4
Dirichi <din206@nyu.edu>
2022-11-20 16:26:44 +0000

    Fall back on gym minigrid wrappers

--- games/gridworld.py
+++ games/gridworld.py
@@ -139,17 +139,17 @@ class MuZeroConfig:
             return 0.25
 
 # Temporary addition until issue with gym minigrid is fixed
-class ImgObsWrapper(ObservationWrapper):
-    """
-    Use the image as the only observation output, no language/mission.
-    """
+# class ImgObsWrapper(ObservationWrapper):
+#     """
+#     Use the image as the only observation output, no language/mission.
+#     """
 
-    def __init__(self, env):
-        super().__init__(env, new_step_api=env.new_step_api)
-        self.observation_space = env.observation_space.spaces["image"]
+#     def __init__(self, env):
+#         super().__init__(env, new_step_api=env.new_step_api)
+#         self.observation_space = env.observation_space.spaces["image"]
 
-    def observation(self, obs):
-        return obs["image"]
+#     def observation(self, obs):
+#         return obs["image"]
 
 
 class Game(AbstractGame):
@@ -159,9 +159,8 @@ class Game(AbstractGame):
 
     def __init__(self, seed=None):
         self.env = gym.make("MiniGrid-Empty-6x6-v0")
-        # Commenting out until issue with gym minigrid wrappers is fixed.
-        # self.env = gym_minigrid.wrappers.ImgObsWrapper(self.env)
-        self.env = ImgObsWrapper(self.env)
+        self.env = gym_minigrid.wrappers.ImgObsWrapper(self.env)
+        # self.env = ImgObsWrapper(self.env)
         if seed is not None:
             self.env.seed(seed)
 
